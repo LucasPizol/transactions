7f207de452cd10c7ccac99271f7384e6ec7ef5b2
Lucas Pizol Ferreira <131599316+LucasPizol@users.noreply.github.com>
2023-05-14 01:53:15 -0300

    Update Firebaseconfig.js

--- src/firebase/Firebaseconfig.js
+++ src/firebase/Firebaseconfig.js
@@ -2,14 +2,7 @@ import * as firebase from "firebase/app";
 import * as fireauth from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIQCMREEdBU8sYNPegNX6sYsfvNYCWgXY",
-  authDomain: "teste-projeto-472e1.firebaseapp.com",
-  databaseURL: "https://teste-projeto-472e1-default-rtdb.firebaseio.com",
-  projectId: "teste-projeto-472e1",
-  storageBucket: "teste-projeto-472e1.appspot.com",
-  messagingSenderId: "983502813772",
-  appId: "1:983502813772:web:5ec222f636c6f5924c52e9",
-  measurementId: "G-K3W22Y29GN"
+  //firebase-config
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
